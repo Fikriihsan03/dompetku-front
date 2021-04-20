810b1ace505475eb0d205f3fd563cde58b59655b
Fikri Ihsan <fikri@192.168.1.32>
2021-04-20 16:37:31 +0700

    get method to spending route

--- src/containers/Pemasukan/IncomeLog.js
+++ src/containers/Pemasukan/IncomeLog.js
@@ -1,18 +1,9 @@
 import { useEffect, useState } from "react";
 
 const IncomeLog = () => {
-  // const sortedActivities = incomeData
-  //   .slice()
-  //   .sort((a, b) => (b.date > a.date ? 1 : -1));
-  // const sortedActivities = incomeData.sort(function (a, b) {
-  //   var aa = a.date.split("/").reverse().join(),
-  //     bb = b.date.split("/").reverse().join();
-  //   return aa < bb ? -1 : aa > bb ? 1 : 0;
-  // });
-  // console.log(sortedActivities.reverse());
   const [incomeData, setIncomeData] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3001/income_log")
+    fetch("http://localhost:3002/income_log")
       .then((res) => res.json())
       .then((data) => {
         setIncomeData(data);
@@ -24,7 +15,7 @@ const IncomeLog = () => {
       <table style={{ textAlign: "center", border: "2px solid black" }}>
         <thead>
           <tr>
-            <td>Tanggal</td>
+            <td>Date</td>
             <td>Total Income</td>
           </tr>
         </thead>
--- src/containers/Pengeluaran/SpendingLog.js
+++ src/containers/Pengeluaran/SpendingLog.js
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
+
 const SpendingLog = () => {
+  const [spendingData, setSpendingData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3002/spending_log")
+      .then((res) => res.json())
+      .then((data) => {
+        setSpendingData(data);
+      });
+  }, []);
   return (
-    <div>
+    <>
       <h1>SpendingLog</h1>
-    </div>
+      <table style={{ textAlign: "center", border: "2px solid black" }}>
+        <thead>
+          <tr>
+            <td>Date</td>
+            <td>Item Name</td>
+            <td>Total Item</td>
+            <td>Total Spending</td>
+          </tr>
+        </thead>
+        <tbody>
+          {spendingData.map((_, i) => {
+            return (
+              <tr key={[i]}>
+                <td>{spendingData[i].spending_date}</td>
+                <td>{spendingData[i].item_name}</td>
+                <td>{spendingData[i].total_items}</td>
+                <td>{spendingData[i].total_spendings}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
 
